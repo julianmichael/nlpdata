737d114584ce4b4f4a16647c9f5773ac01a46277
julianmichael <julianjohnmichael@gmail.com>
2017-09-12 19:45:10 -0700

    Add VerbForm abstraction

--- nlpdata/shared/src/main/scala/nlpdata/datasets/wiktionary/InflectedForms.scala
+++ nlpdata/shared/src/main/scala/nlpdata/datasets/wiktionary/InflectedForms.scala
@@ -8,6 +8,37 @@ case class InflectedForms(
   present: LowerCaseString,
   presentParticiple: LowerCaseString,
   past: LowerCaseString,
-  pastParticiple: LowerCaseString) {
+  pastParticiple: LowerCaseString
+) extends (VerbForm => LowerCaseString) {
+
+  def apply(form: VerbForm): LowerCaseString = form match {
+    case Stem => stem
+    case PresentSingular3rd => present
+    case PresentParticiple => presentParticiple
+    case Past => past
+    case PastParticiple => pastParticiple
+  }
+
   def allForms: List[LowerCaseString] = List(stem, present, presentParticiple, past, pastParticiple)
+}
+
+object InflectedForms {
+  def fromStrings(stem: String, present: String, presentParticiple: String, past: String, pastParticiple: String) =
+    InflectedForms(
+      stem.lowerCase,
+      present.lowerCase,
+      presentParticiple.lowerCase,
+      past.lowerCase,
+      pastParticiple.lowerCase)
+
+  val doForms = InflectedForms.fromStrings(
+    "do", "does", "doing", "did", "done")
+
+  val beSingularForms = InflectedForms.fromStrings(
+    "be", "is", "being", "was", "been")
+
+  val haveForms = InflectedForms.fromStrings(
+    "have", "has", "having", "had", "had"
+  )
+
 }
---
+++ nlpdata/shared/src/main/scala/nlpdata/datasets/wiktionary/VerbForm.scala
@@ -0,0 +1,15 @@
+package nlpdata.datasets.wiktionary
+
+sealed trait VerbForm
+case object Stem extends VerbForm
+case object PresentSingular3rd extends VerbForm
+case object PresentParticiple extends VerbForm
+case object Past extends VerbForm
+case object PastParticiple extends VerbForm
+
+object VerbForm {
+  def allForms: List[VerbForm] = List(
+    Stem,
+    PresentSingular3rd, PresentParticiple,
+    Past, PastParticiple)
+}
