61ee11ae1a357a8dac8ef0b778a27ea4b49bc1d2
julianmichael <julianjohnmichael@gmail.com>
2017-10-03 17:58:28 -0700

    Make HasAlignedTokens extend and implement HasTokens

--- nlpdata/shared/src/main/scala/nlpdata/util/HasAlignedTokens.scala
+++ nlpdata/shared/src/main/scala/nlpdata/util/HasAlignedTokens.scala
@@ -8,7 +8,9 @@ import scala.language.implicitConversions
 /*
  * Typeclass for types that can be rendered as a list of Penn-Treebank style tokens.
  */
-@typeclass trait HasAlignedTokens[-A] {
+@typeclass trait HasAlignedTokens[-A] extends HasTokens[A] {
   /* Returns a vector of Penn Treebank style tokens. */
   @op("alignedTokens") def getAlignedTokens(a: A): Vector[AlignedToken]
+
+  override def getTokens(a: A): Vector[String] = getAlignedTokens(a).map(_.token)
 }
